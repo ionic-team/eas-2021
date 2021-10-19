185ade8144413af4e46ffbb0a3a79a827ac92b5c
Matt Netkow <matt.netkow@gmail.com>
2021-10-19 14:54:21 -0500

    Add Capacitor ios/android and Appflow plugin

---
+++ android/app/src/main/java/io/ionic/conferences/eas2021/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.conferences.eas2021;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.conferences.eas2021',
   appName: 'enterprise-summit-2021',
   webDir: 'www',
   bundledWebRuntime: false
