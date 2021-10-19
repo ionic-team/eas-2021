f6038d6ef9dae595e1b65582103d268e4fd95707
Matt Netkow <matt.netkow@gmail.com>
2021-10-19 14:19:14 -0500

    Add Angular app and Ionic v6 RC0

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'enterprise-summit-2021',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
