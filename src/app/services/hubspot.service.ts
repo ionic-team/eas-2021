a89271d41cbf9d549ff16f7d9ebdeda3d8d786b0
Matt Netkow <matt.netkow@gmail.com>
2021-11-08 21:29:12 -0600

    switch to hubspot api

---
+++ src/app/services/hubspot.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HubspotService  {
+
+  FORM_SUBMIT_URL = "https://api.hsforms.com/submissions/v3/integration/submit/3776657/75127e93-3da1-4368-b3a7-c680d53483ac";
+  
+  public async submitToHubspot() {
+    const data = {
+      "fields": [
+        {
+          "name": "firstname",
+          "value": "Matteo2"
+        },
+        {
+          "name": "lastname",
+          "value": "Guapo2"
+        },
+        {
+          "name": "email",
+          "value": "matteo@guapo.com"
+        },
+        {
+          "name": "address",
+          "value": "555 Fake St."
+        },
+        {
+          "name": "city",
+          "value": "Madison"
+        },
+        {
+          "name": "zip",
+          "value": "53711"
+        },
+        {
+          "name": "country_pl_",
+          "value": "United States"
+        },
+        {
+          "name": "state",
+          "value": "Alaska"
+        },
+        {
+          "name": "t_shirt_size",
+          "value": "Large"
+        }
+      ]
+    };
+
+    const response = await fetch(this.FORM_SUBMIT_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+    console.log(response);
+    }
+}
--- src/app/swag/swag.page.ts
+++ src/app/swag/swag.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HubspotService } from '../services/hubspot.service';
 
 @Component({
   selector: 'app-swag',
@@ -7,35 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwagPage implements OnInit {
 
-  constructor() { }
+  constructor(private hubspotService: HubspotService) { }
 
   ngOnInit() {
-    this.loadScript();
+    
   }
 
-  private loadScript() {
-    let hubspotScript = document.createElement("script");
-    hubspotScript.type = "text/javascript";
-    hubspotScript.async = false;
-    hubspotScript.src = "https://js.hsforms.net/forms/v2.js";
-    document.body.appendChild(hubspotScript);
-
-    hubspotScript.onload = () => {
-      this.createForm();
-    }
-  }
-
-  public createForm() {
-    (window as any).hbspt.forms.create({
-      portalId: '3776657',
-      formId: "75127e93-3da1-4368-b3a7-c680d53483ac",
-      region: "na1",
-      target: `#hubspot-form`,
-      translations: {
-        en: {
-          submitText: "Submit ",
-        },
-      },
-    });
+  public async submit() {
+    await this.hubspotService.submitToHubspot();
   }
 }
