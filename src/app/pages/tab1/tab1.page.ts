3128df6e76d912c3c4b930854c315e1a389117fa
Matt Netkow <matt.netkow@gmail.com>
2021-11-30 11:36:14 -0600

    update(push): Add Android icon and push notification fix

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -5,6 +5,7 @@ import { ModalController } from '@ionic/angular';
 import { PermissionsModalComponent } from 'src/app/components/permissions-modal/permissions-modal.component';
 import { Capacitor } from '@capacitor/core';
 import { PushNotificationService } from 'src/app/services/push-notification.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-tab1',
@@ -16,7 +17,8 @@ export class Tab1Page {
 
   constructor(private agendaService: AgendaService,
     private modalController: ModalController,
-    private pushNotificationService: PushNotificationService) {
+    private pushNotificationService: PushNotificationService,
+    private storageService: StorageService) {
     this.agenda = this.agendaService.getAgenda();
   }
 
@@ -33,10 +35,15 @@ export class Tab1Page {
     // Show permission prompt the first time app is launched
     if (Capacitor.getPlatform() !== 'web') {
       const permStatus = await this.pushNotificationService.checkPermissionStatus();
+
       if (permStatus === 'granted') {
+        // On Android, permission is granted automatically
         await this.pushNotificationService.registerPush();
       }
-      else if (permStatus === 'prompt') {
+      else if (permStatus === 'prompt' && !(await this.storageService.getPushNotesModalShown())) {
+        // On iOS, ask the user for permission first. only once.
+        await this.storageService.setPushNotesModalShown();
+        
         const modal = await this.modalController.create({
           component: PermissionsModalComponent,
           initialBreakpoint: 1,
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService  {
+  async setPushNotesModalShown() {
+    await Storage.set({
+      key: "pushmodalshown",
+      value: "true"
+    });
+  }
+
+  async getPushNotesModalShown() {
+    const { value } = await Storage.get({ key: "pushmodalshown"});
+    return value;
+  }
+}
